7aeffafab6ee043fa66535cd1e2d68dabe4ccc1b
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2022-11-15 19:05:58 +0800

    Default item changes

--- midterm/lib/model/todo.dart
+++ midterm/lib/model/todo.dart
@@ -11,8 +11,8 @@ class ToDo {
       ToDo(id: '01', todoText: 'Morning Exercise', isDone: true),
       ToDo(id: '02', todoText: 'Buy Groceries', isDone: true),
       ToDo(id: '03', todoText: 'Check Emails'),
-      ToDo(id: '04', todoText: 'Check Emails'),
-      ToDo(id: '05', todoText: 'Check Emails'),
+      ToDo(id: '04', todoText: 'Feed cat'),
+      ToDo(id: '05', todoText: 'Charge battery'),
       ToDo(id: '06', todoText: 'Attend birthday party'),
     ];
   }
